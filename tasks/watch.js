0f9b10021ec47850ee5f68bb63adb04b419527d6
Bobby Johnson <popcorn245@gmail.com>
2016-10-22 01:26:54 -0500

    Add gulp-watch

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,10 +1,28 @@
+const watch = require('gulp-watch');
+const runSequence = require('run-sequence');
+
+
 module.exports = function(gulp, callback) {
     global.isWatching = true;
-    gulp.watch(configFile, ['config:build']);
-    gulp.watch(cssWatch, ['css:build']);
-    gulp.watch(jsWatch, ['js:lint']);
-    gulp.watch(jsLib, ['js:libraries']);
-    gulp.watch(htmlWatch, ['html:compile']);
-    gulp.watch(htmlTemplate, ['html:template']);
-    //gulp.watch(imgWatch, ['img:icon']);
+    gulp.watch(configFile, function(callback) {
+        runSequence('config:build', callback);
+    });
+    gulp.watch(cssWatch, function(callback){
+        runSequence('css:build', callback);
+    });
+    gulp.watch(jsWatch, function(callback){ 
+        runSequence('js:lint', callback);
+    });
+    gulp.watch(jsLib, function(callback){
+        runSequence('js:libraries', callback)
+    });
+    gulp.watch(htmlWatch, function(callback) {
+        runSequence('html:compile', callback);
+    });
+    gulp.watch(htmlTemplate, function(callback) {
+        runSequence('html:template', callback);
+    });
+    gulp.watch(imgWatch, function(callback){
+        runSequence('img:icon');
+    });
 };
