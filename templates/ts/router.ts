c3009127d223fc104fb8214d836ba7b462f46cd9
Bobby Johnson <popcorn245@gmail.com>
2016-10-21 21:27:12 -0500

    First Commit

---
+++ templates/ts/router.ts
@@ -0,0 +1,19 @@
+/// <reference path="../typings/index.d.ts"/>
+module <%= app %> {
+    'use strict';
+
+    class <%= app %>Router {
+        constructor($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                <%= routes %>;
+
+            $urlRouterProvider.otherwise(function($injector, $location) {
+                var $state = $injector.get('$state');
+                $state.go('<%= defaultRoute %>');
+            });
+        }
+    }
+
+    angular.module('<%= app %>')
+           .config(<%= app %>Router);
+}
